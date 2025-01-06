a27620a8b3e5dca175149d04153c67ccbb30ea76
Kevin Balke <fughilli@gmail.com>
2025-01-05 23:02:14 -0800

    Full white

---
+++ full_white_scene.py
@@ -0,0 +1,14 @@
+from artnet import Scene, RGB
+
+
+# Blink each of the layers on and off in sequence.
+class FullWhiteScene(Scene):
+
+    def render(self, raster, time):
+        for y in range(raster.height):
+            for x in range(raster.width):
+                for z in range(raster.length):
+                    idx = y * raster.width + x + z * raster.width * raster.height
+                    dot_on = int(idx + time * 30) % 4 == 0
+                    raster.data[idx] = RGB(255, 255, 255) if dot_on else RGB(
+                        0, 0, 0)
